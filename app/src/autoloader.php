fe796722514a9820ba7627bd6739504cb0631d1d
John Kelly <john.kelly@theperfectworkout.com>
2023-05-06 20:57:18 -0500

    update autoloader

--- app/src/autoloader.php
+++ app/src/autoloader.php
@@ -4,21 +4,23 @@
 require_once APP_DIRECTORY . "/src/lib/exception/MissingClassException.php";
 
 use exception\MissingClassException;
+use src\controllers\AppController;
 use src\lib\AppError;
 
-spl_autoload_register(function ($class) {
+spl_autoload_register(function ($class)
+{
     $originalClass = $class;
     try {
         $class = str_replace('\\', '/', $class);
-
+        
         $possiblePaths = [
             '/',
             '/src/',
+            '/src/lib/',
             '/controller/',
             '/lib/',
             '/model/',
             '/plugin/',
-            '/exception/',
         ];
         foreach ($possiblePaths as $possiblePath) {
             $fullClassPath = APP_DIRECTORY . $possiblePath . $class . ".php";
@@ -30,5 +32,8 @@ spl_autoload_register(function ($class) {
         throw new MissingClassException("Unable to find lib: " . $class);
     } catch (MissingClassException $e) {
         AppError::log($e->getMessage(), ['lib' => $originalClass]);
+        $Controller = new AppController();
+        $Controller->display($Controller->internalServerError($e));
+        exit;
     }
 });
