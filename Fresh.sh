5a931d1d6901401875dce4da74ccd26c56c211c8
John Kelly <john.kelly@theperfectworkout.com>
2023-05-18 20:07:10 -0500

    change shell script base directory

--- Fresh.sh
+++ Fresh.sh
@@ -8,14 +8,14 @@ clean_project() {
   y | Y)
     directories=("controller" "lib" "model" "scripts" "styles" "views" "css")
     for _directory in "${directories[@]}"; do
-      rm -R "$_directory"
+      rm -R "app/$_directory"
     done
     files=(".htaccess" ".config.json" "index.php" "config.php")
     for _file in "${files[@]}"; do
-      rm "$_file"
+      rm "app/$_file"
     done
 
-    getting_started_directory="src/_getting-started/"
+    getting_started_directory="app/src/_getting-started/"
     find "$getting_started_directory" -type f | while read -r file; do
         new_name="${file%.template}"
         mv "$file" "$new_name"
@@ -37,16 +37,16 @@ create_project() {
 
   for _directory in "${directories[@]}"; do
     echo "Creating '$_directory' directory."
-    mkdir -p "$_directory"
+    mkdir -p "app/$_directory"
   done
 
   getting_started=(".config.json" ".htaccess" "index.php" "config.php" "model/UserModel.php" "views/template/webpage.tpl" "views/template/nav-menu.tpl" "views/template/footer.tpl" "views/components/select.tpl" "views/components/submit.tpl" "views/components/table.tpl" "views/components/text.tpl" "views/App/forbidden.tpl" "views/App/internal-server-error.tpl" "views/App/page-not-found.tpl" "views/Home/index.tpl" "controller/HomeController.php" "css/app.css" "lib/AppError.php")
   for file in "${getting_started[@]}"; do
     echo "Loading file '$file'."
-    cp src/_getting-started/__"$file" ./"$file"
+    cp app/src/_getting-started/__"$file" "app/$file"
   done
 
-  getting_started_directory="src/_getting-started/"
+  getting_started_directory="app/src/_getting-started/"
 
   find "$getting_started_directory" -type f | while read -r file; do
       new_name="${file%}.template"
